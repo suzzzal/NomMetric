7e6d77dde0a2b44414661dbfe8c0df82109c76b7
Hasan <mhasanravda@gmail.com>
2025-12-30 15:24:39 +0530

    created a placeholder homescreen

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Screen!'),
+      ),
+    );
+  }
+}
